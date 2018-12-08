6dfc38f9b3f473831c93f76eefb0a29782ae3701
Jorge Ibarra <jorgeibarra1012@gmail.com>
2018-12-07 23:25:39 -0600

    Updates

---
+++ precache-manifest.5d5f291361c7e0f66001caa97de32f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/web_sandbox/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "9a57ab29c808b3ed18f2",
+    "url": "/web_sandbox/static/js/runtime~main.9a57ab29.js"
+  },
+  {
+    "revision": "b40b247cbc8f67dfcf97",
+    "url": "/web_sandbox/static/js/main.b40b247c.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/web_sandbox/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "b40b247cbc8f67dfcf97",
+    "url": "/web_sandbox/static/css/main.1db1b83f.chunk.css"
+  },
+  {
+    "revision": "42520cd00fa2c2d371283a48d3f87ade",
+    "url": "/web_sandbox/index.html"
+  }
+];
